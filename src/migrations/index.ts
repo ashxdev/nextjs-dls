1d1c728658e7605dbfbb3ecc84af54916ac3eddf
Andrii Shapovalov <ashxdev@gmail.com>
2024-12-01 00:37:20 +0200

    up packages

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241130_185707_init from './20241130_185707_init';
-
-export const migrations = [
-  {
-    up: migration_20241130_185707_init.up,
-    down: migration_20241130_185707_init.down,
-    name: '20241130_185707_init'
-  },
-];
