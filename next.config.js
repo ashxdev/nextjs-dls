b9cc23b7943a2a9362834ce93c556f43a5243310
Andrii Shapovalov <ashxdev@gmail.com>
2024-12-05 10:06:56 +0200

    up next image config

--- next.config.js
+++ next.config.js
@@ -9,16 +9,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
+    domains: ['dance-line.studio'],
   },
   reactStrictMode: true,
   redirects,
