ab7613dd12139d21675f2a6f52ac23cf1a6c1987
Andrii Shapovalov <ashxdev@gmail.com>
2024-12-09 19:44:22 +0200

    rall back imageMedia

--- src/components/marketing/Media/ImageMedia/index.tsx
+++ src/components/marketing/Media/ImageMedia/index.tsx
@@ -8,8 +8,10 @@ import React from 'react'
 
 import type { Props as MediaProps } from '../types'
 
+import { cssVariables } from '@/cssVariables'
 import { getClientSideURL } from '@/utilities/getURL'
 
+const { breakpoints } = cssVariables
 
 // A base64 encoded image to use as a placeholder while the image is loading
 const placeholderBlur =
@@ -50,19 +52,29 @@ export const ImageMedia: React.FC<MediaProps> = (props) => {
 
   const loading = loadingFromProps || 'lazy'
 
+  // NOTE: this is used by the browser to determine which image to download at different screen sizes
+  const sizes = sizeFromProps
+    ? sizeFromProps
+    : Object.entries(breakpoints)
+      .map(([, value]) => `(max-width: ${value}px) ${value * 2}w`)
+      .join(', ')
+
   return (
-    <NextImage
-      alt={alt || ''}
-      className={cn(imgClassName)}
-      fill={fill}
-      height={!fill ? height : undefined}
-      placeholder="blur"
-      blurDataURL={placeholderBlur}
-      priority={priority}
-      quality={100}
-      loading={loading}
-      src={src}
-      width={!fill ? width : undefined}
-    />
+    <picture>
+      <NextImage
+        alt={alt || ''}
+        className={cn(imgClassName)}
+        fill={fill}
+        height={!fill ? height : undefined}
+        placeholder="blur"
+        blurDataURL={placeholderBlur}
+        priority={priority}
+        quality={100}
+        loading={loading}
+        sizes={sizes}
+        src={src}
+        width={!fill ? width : undefined}
+      />
+    </picture>
   )
 }
